fa4d07bab56554be8507f1af02f3aeac2d554d5a
Neil Dixit <neil.s.dixit@gmail.com>
2026-09-16 22:02:47 +0530

    Fix Metro cache issue: add react-native-css-interop/.cache to watchFolders

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,14 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+// Ensure the react-native-css-interop cache directory is watched
+config.watchFolders = [
+  ...(config.watchFolders || []),
+  path.join(__dirname, "node_modules", "react-native-css-interop", ".cache"),
+];
 
 // Disable cache for problematic modules
 config.cacheStores = [];
